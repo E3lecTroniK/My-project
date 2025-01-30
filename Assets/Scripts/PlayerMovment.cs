636e043810adbada7b8f8cc4c1e45da18dbc6c86
Nevo Baron <nevobaron2001@gmail.com>
2025-01-30 09:59:28 +0200

    added nothing

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -9,12 +9,20 @@ public class PlayerMovement : MonoBehaviour
     float jumpPower = 5f;
     bool isGrounded = false;
 
+
+    // For double jump
+    int jumpCount = 0;
+    [SerializeField] int maxJumps = 2;
+
+    // Animator parameters
+    // isJumping = true while moving upward
+    // isFalling = true while moving downward
+
     Vector2 startPosition;
 
     Rigidbody2D rb;
     Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,18 +30,34 @@ public class PlayerMovement : MonoBehaviour
         startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-
         FlipSprite();
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        // Check if jump key was pressed and we haven't exceeded max jumps
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumps)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            jumpCount++;
             isGrounded = false;
-            animator.SetBool("isJumping", !isGrounded);
+            animator.SetBool("isJumping", true);
+            animator.SetBool("isFalling", false);
+        }
+
+        // Update animator states based on y-velocity
+        if (!isGrounded)
+        {
+            if (rb.velocity.y > 0)
+            {
+                animator.SetBool("isJumping", true);
+                animator.SetBool("isFalling", false);
+            }
+            else if (rb.velocity.y < 0)
+            {
+                animator.SetBool("isJumping", false);
+                animator.SetBool("isFalling", true);
+            }
         }
     }
 
@@ -46,7 +70,7 @@ public class PlayerMovement : MonoBehaviour
 
     void FlipSprite()
     {
-        if (isFacingLeft && horizontalInput < 0f || !isFacingLeft && horizontalInput > 0f)
+        if ((isFacingLeft && horizontalInput < 0f) || (!isFacingLeft && horizontalInput > 0f))
         {
             isFacingLeft = !isFacingLeft;
             Vector3 ls = transform.localScale;
@@ -57,14 +81,15 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // When player lands on the ground
         isGrounded = true;
-        animator.SetBool("isJumping", !isGrounded);
+        jumpCount = 0;
+        animator.SetBool("isJumping", false);
+        animator.SetBool("isFalling", false);
     }
 
     public void Die()
     {
         transform.position = startPosition;
     }
-
 }
-
