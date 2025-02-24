2468afcc5087aa900b33afa5398d690100859b15
Nevo Baron <nevobaron2001@gmail.com>
2025-02-24 10:57:35 +0200

    i didnt fix it even with help!

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,22 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PausedMenu : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
-    public GameObject PauseMenu;
+    public GameObject pauseMenu;
 
     public static bool isPaused;
 
-
-
     void Start()
     {
-        PauseMenu.SetActive(false);
+        pauseMenu.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isPaused)
             {
@@ -34,14 +32,14 @@ public class PausedMenu : MonoBehaviour
 
     public void PauseGame()
     {
-        PauseMenu.SetActive(true);
+        pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
     }
 
     public void ResumeGame()
     {
-        PauseMenu.SetActive(false);
+        pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
     }
